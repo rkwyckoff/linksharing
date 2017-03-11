f17e059c3da790a65796b443c7ec9f90e4d46ce2
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-11 13:10:46 -0500

    initial routes made and db migrated

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,15 @@
-// Require your controllers here
+const middleware = require("../middleware");
+
+const UserController = require("../controllers/user");
 
 
 module.exports = (app) => {
-  // Add your routes here
+  app.post('/users', UserController.register);
+  app.post('/login', UserController.login);
+
+  //app.post('/shelters', middleware.authenticate, ShelterController.create);
+//  app.get('/shelters/:id', ShelterController.listAnimals);
+
+//  app.post('/shelters/:shelterId/animals', AnimalController.create);
+//  app.put('/shelters/:shelterId/animals/:id', AnimalController.update);
 };
